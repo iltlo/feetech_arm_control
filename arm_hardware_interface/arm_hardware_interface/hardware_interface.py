5498e410de1eeb7a135702e0712e47f47e76a865
iltlo <iltlo@connect.hku.hk>
2025-02-14 01:03:59 +0800

    Implement real joint state publisher

--- arm_hardware_interface/arm_hardware_interface/hardware_interface.py
+++ arm_hardware_interface/arm_hardware_interface/hardware_interface.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from arm_hardware_interface.motors.feetech import FeetechMotorsBus
+import time
+from serial.serialutil import SerialException
 
 class HardwareInterface(Node):
     def __init__(self):
@@ -11,6 +13,9 @@ class HardwareInterface(Node):
             'joint_states',
             self.joint_state_callback,
             10)
+        self.publisher_ = self.create_publisher(JointState, 'real_joint_states', 10)
+        self.timer = self.create_timer(0.02, self.publish_real_joint_states)  # 0.02 -> 50 Hz
+
         self.motors_bus = FeetechMotorsBus(
             port="/dev/ttyACM0",
             motors={},
@@ -61,7 +66,33 @@ class HardwareInterface(Node):
 
         if motor_ids:
             self.motors_bus.write_with_motor_ids(motor_models, motor_ids, "Goal_Position", motor_values)
-            self.get_logger().info(f"Joint positions: {list(zip(msg.name, motor_values))}")
+            # self.get_logger().info(f"Joint positions: {list(zip(msg.name, motor_values))}")
+
+    def publish_real_joint_states(self):
+        motor_ids = list(self.joint_to_motor_id.values())
+        motor_models = ["sts3215"] * len(motor_ids)
+        try:
+            positions = self.motors_bus.read_with_motor_ids(motor_models, motor_ids, "Present_Position")
+        except (ConnectionError, SerialException) as e:
+            self.get_logger().error(f"Connection error: {e}")
+            self.get_logger().info("Attempting to reconnect...")
+            while True:
+                try:
+                    self.motors_bus.reconnect()
+                    self.get_logger().info("Reconnected to motors.")
+                    return
+                except Exception as e:
+                    self.get_logger().error(f"Reconnection failed: {e}")
+                    time.sleep(1)  # Wait for 1 second before retrying
+
+        joint_state_msg = JointState()
+        joint_state_msg.header.stamp = self.get_clock().now().to_msg()
+        joint_state_msg.name = self.motor_names
+        joint_state_msg.position = [((-pos / 4095.0) * (2 * 3.14) - 3.14) for pos in positions]  # Convert motor value to radians
+
+        self.publisher_.publish(joint_state_msg)
+        # self.get_logger().info(f"Published real joint states: {positions}")
+        self.get_logger().info(f"Published real joint states (radians): {joint_state_msg.position}")
 
 def main(args=None):
     rclpy.init(args=args)
